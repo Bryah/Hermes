7441f1c36205d81f21a411fb3d8e8550de0e832a
Andrew Christensen <andrew@emergingdesigns.com>
2019-10-07 19:15:45 -0600

    cccccccchanges

--- src/models/UserTrait.php
+++ src/models/UserTrait.php
@@ -9,9 +9,68 @@ trait UserTrait {
         return "bar";
     }
 
-    public function conversations()
+    public function conversations($messageType=null,$start=0,$limit=20)
     {
-        return $this->belongsToMany(EloquentBase::modelPath('Conversation'), EloquentBase::tableName('conversation_user'))->withTimestamps()->orderBy('updated_at', 'desc');
+        if (!$messageType) {
+            return $this->belongsToMany(EloquentBase::modelPath('Conversation'),
+                                        EloquentBase::tableName('conversation_user'))
+                                        ->withTimestamps()->orderBy('updated_at', 'desc');
+        } else {
+            $conversationUsersModel = EloquentBase::modelPath('Conversation');
+            $messagesModel = EloquentBase::modelPath('Message');
+
+            $usersConversationIds = \App\ConversationUser::where('user_id',$this->id)
+                                                            ->get()
+                                                            ->lists('conversation_id');
+
+            switch ($messageType) {
+                case "received":
+                    foreach($usersConversationIds as $cId) {
+                        $messages = $messagesModel::whereIn('conversation_id',$usersConversationIds)
+                                                    ->where('user_id','!=',$this->id)
+                                                    ->orderBy('updated_at', 'desc')
+                                                    ->get();
+                    }
+                    break;
+                case "sent":
+                    foreach($usersConversationIds as $cId) {
+                        $messages = $messagesModel::whereIn('conversation_id',$usersConversationIds)
+                                                    ->where('user_id',$this->id)
+                                                    ->orderBy('updated_at', 'desc')
+                                                    ->get();
+                    }
+                    break;
+                case "archived":
+                    foreach($usersConversationIds as $cId) {
+                        $allMessages = $messagesModel::whereIn('conversation_id',$usersConversationIds)
+                                                    ->get();
+                        $messageIds = [];
+                        foreach($allMessages as $aM) {
+                            if ($aM->messageState($this->id) == 3) {
+                                $messageIds[] = $aM->id;
+                            }
+                        }
+                        $messages = $messagesModel::whereIn('id',$messageIds)
+                                                    ->orderBy('updated_at', 'desc')
+                                                    ->get();
+                    }
+                    break;
+            }
+
+            $filteredConversationIds = [];
+            foreach($messages as $m) {
+                $filteredConversationIds[] = $m->conversation_id;
+            }
+
+            return $this->belongsToMany(EloquentBase::modelPath('Conversation'),
+                                        EloquentBase::tableName('conversation_user'))
+                                        ->whereIn('conversation_id',$filteredConversationIds)
+                                        ->withTimestamps()
+                                        ->orderBy('updated_at', 'desc')
+                                        ->skip($start)
+                                        ->take($limit);
+        }
+
     }
 
 	public function messageStates()
